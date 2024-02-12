236868fd26970dd0bd070d0bda66fa6bbd49e4bc
Dark12Phantom <144307615+Dark12Phantom@users.noreply.github.com>
2024-02-12 09:10:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'development';
+const isWin = process.platform === 'win32';
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow(
+        {
+            title: 'Music Player',
+            width: 1360,
+            height: 760
+        }
+    );
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './src/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (!isWin) {
+        app.quit()
+    }
+})
